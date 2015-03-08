6c95d93e3d162c1c22e89dae1d1a722882e9e240
Magnus Bae <magnus.bae@itera.no>
2015-03-08 21:53:16 +0100

    Added ant dependency to install script

--- install.sh
+++ install.sh
@@ -16,4 +16,11 @@ tar --strip-components=1 -zxf leJOS_EV3_0.9.0-beta.tar.gz
 echo "cleaning up"
 rm -f leJOS_EV3_0.9.0-beta.tar.gz
 echo "Done. Lejos installed to ~/.lejos"
+echo ""
+echo "Downloading Ant dependency to ~/.ant/lib"
+cd ~
+mkdir -p .ant/lib
+cd .ant/lib
+curl -O https://raw.githubusercontent.com/magnusbae/HelloBrick/install-script/jsch-0.1.51.jar
+echo "Ant dependency installed. Is Apache Ant installed on your system?"
 
